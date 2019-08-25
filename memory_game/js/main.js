a4c43818fc5735ee1520a0e2330ac2db47738738
Raul Jiminian <RJiminian@iBooks-MacBook-Pro.local>
2019-08-25 16:00:39 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,23 +24,35 @@ var cards = [
 ];
 var cardsInPlay = [];
 
-function checkForMatch() {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 }
 
-function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+var flipCard = function() {
+	cardId = this.getAttribute('data-id');
+	console.log(cardId);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
-	};
+		cardInPlay = [];
+	}
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+}
+
+createBoard();
